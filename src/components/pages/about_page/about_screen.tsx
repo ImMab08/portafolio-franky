d80e054b4ef892db409741aecc5b4a7308247932
mab08 <frankyvargas15@gmail.com>
2026-01-28 20:53:20 -0500

    Actualización about-me v0.3

--- src/components/pages/about_page/about_screen.tsx
+++ src/components/pages/about_page/about_screen.tsx
@@ -34,7 +34,7 @@ const AboutScreen = () => {
               {tAbout("subtitle")}{" "}
               <span className="text-tertiary">{tAbout("subtitle-span")}</span>
             </h2>
-            <p className="text-base md:text-xl text-gray-300 md:pr-32">
+            <p className="text-base md:text-lg text-gray-300 md:pr-20">
               {tAbout("description")}
             </p>
           </motion.div>
@@ -73,56 +73,54 @@ const AboutScreen = () => {
         </motion.div>
       </div>
 
-      <div className="h-screen md:h-auto w-full overflow-auto">
+      <div className="h-screen md:h-auto w-full md:w-auto md:max-w-2xl relative flex overflow-auto">
         {/* Horizontal timeline container */}
 
-        <div className="relative">
-          {/* Timeline line */}
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-            className="absolute left-0 right-0 top-1/2 h-0.5 bg-gradient-to-r from-secondary via-emerald-500 to-green-400 transform -translate-y-1/2 z-0"
-          ></motion.div>
+        {/* Timeline line */}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="absolute left-0 right-0 top-1/2 h-0.5 bg-gradient-to-r from-tertiary via-emerald-500 to-green-400 transform -translate-y-1/2 z-0"
+        ></motion.div>
 
-          {/* Scrollable container */}
-          <motion.div
-            className="flex overflow-auto scrollbar-hide"
-            ref={containerRef}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.8, delay: 0.4 }}
-          >
-            <div className="flex gap-16 px-0 md:px-8 min-w-max bg-red">
-              <div className="w-5 h-full bg-primary absolute top-0 left-0 z-40 mask-radial-[100%_100%] mask-radial-from-40% mask-radial-at-left"></div>
-              {experience.map((item: ExperienceItem, index) => {
-                const Icon = item.icon;
-                return (
-                  <TimeLine
-                    key={item.id}
-                    item={{
-                      ...item,
-                      type: tExperience(`${index}.type`),
-                      title: tExperience(`${index}.title`),
-                      organization: tExperience(`${index}.organization`),
-                      period: tExperience(`${index}.period`),
-                      location: tExperience(`${index}.location`),
-                      description: tExperience(`${index}.description`),
-                      skills: item.skills.map((_, i) =>
-                        tExperience(`${index}.skills.${i}`),
-                      ),
-                    }}
-                    index={index}
-                    Icon={Icon}
-                    isActive={activeIndex === index}
-                    setActive={() => setActiveIndex(index)}
-                  />
-                );
-              })}
-              <div className="w-5 h-full bg-primary absolute top-0 right-0 z-40 mask-radial-[100%_100%] mask-radial-from-40% mask-radial-at-right"></div>
-            </div>
-          </motion.div>
-        </div>
+        {/* Scrollable container */}
+        <motion.div
+          className="flex overflow-auto scrollbar-hide"
+          ref={containerRef}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.4 }}
+        >
+          <div className="flex gap-16 px-0 md:px-8 min-w-max bg-red">
+            <div className="w-5 h-full bg-primary absolute top-0 left-0 z-40 mask-radial-[100%_100%] mask-radial-from-40% mask-radial-at-left"></div>
+            {experience.map((item: ExperienceItem, index) => {
+              const Icon = item.icon;
+              return (
+                <TimeLine
+                  key={item.id}
+                  item={{
+                    ...item,
+                    type: tExperience(`${index}.type`),
+                    title: tExperience(`${index}.title`),
+                    organization: tExperience(`${index}.organization`),
+                    period: tExperience(`${index}.period`),
+                    location: tExperience(`${index}.location`),
+                    description: tExperience(`${index}.description`),
+                    skills: item.skills.map((_, i) =>
+                      tExperience(`${index}.skills.${i}`),
+                    ),
+                  }}
+                  index={index}
+                  Icon={Icon}
+                  isActive={activeIndex === index}
+                  setActive={() => setActiveIndex(index)}
+                />
+              );
+            })}
+            <div className="w-5 h-full bg-primary absolute top-0 right-0 z-40 mask-radial-[100%_100%] mask-radial-from-40% mask-radial-at-right"></div>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
