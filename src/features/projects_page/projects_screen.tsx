252f57eef752ece2b8a20dbdb4e3aded797e97b2
mab08 <frankyvargas15@gmail.com>
2026-02-01 23:15:03 -0500

    Actualización página de proyectos v0.2

--- src/features/projects_page/projects_screen.tsx
+++ src/features/projects_page/projects_screen.tsx
@@ -1,8 +1,9 @@
+import { BuildingScreen } from "../building_screen";
 
 const ProjectsScreen = () => {
   return (
     <div className="flex flex-col size-full flex-1 items-center justify-center px-5 md:px48">
-      
+      <BuildingScreen />
     </div>
   );
 };
